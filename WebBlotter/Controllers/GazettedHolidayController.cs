6d42e96367fafb14c9ab4d7d6e83e60775c44e3d
zohaib4m2010 <zohaib4m2010@gmail.com>
2021-10-29 15:09:10 +0500

    update

---
+++ WebBlotter/Controllers/GazettedHolidayController.cs
@@ -0,0 +1,268 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebBlotter.Classes;
+using WebBlotter.Models;
+using WebBlotter.Repository;
+
+namespace WebBlotter.Controllers
+{
+    [AuthAccess]
+    public class GazettedHolidayController : Controller
+    {
+
+        public ActionResult GazettedHoliday(FormCollection form)
+        {
+            #region Added by shakir (Currency parameter)
+            var selectCurrency = (dynamic)null;
+
+            if (form["selectCurrency"] != null)
+                selectCurrency = Convert.ToInt32(form["selectCurrency"].ToString());
+            else
+                selectCurrency = Convert.ToInt32(Session["SelectedCurrency"].ToString());
+
+            UtilityClass.GetSelectedCurrecy(selectCurrency);
+
+            #endregion
+
+            ServiceRepository serviceObj = new ServiceRepository();
+            HttpResponseMessage response = serviceObj.GetResponse("/api/GazettedHolidays/GetAllBlotterGH?UserID=" + Session["UserID"].ToString());
+            response.EnsureSuccessStatusCode();
+            List<Models.SP_GetSBPBlotterGH_Result> blotterGH = new List<Models.SP_GetSBPBlotterGH_Result>();
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                var JsonLinq = JObject.Parse(jsonResponse);
+                WebApiResponse getreponse = new WebApiResponse();
+                getreponse.Status = Convert.ToBoolean(JsonLinq["Status"]);
+                getreponse.Message = JsonLinq["Message"].ToString();
+                getreponse.Data = JsonLinq["Data"].ToString();
+
+                if (getreponse.Message == "Success")
+                {
+                    JavaScriptSerializer ser = new JavaScriptSerializer();
+                    Dictionary<string, dynamic> ResponseDD = ser.Deserialize<Dictionary<string, dynamic>>(JsonLinq.ToString());
+                    blotterGH = JsonConvert.DeserializeObject<List<Models.SP_GetSBPBlotterGH_Result>>(ResponseDD["Data"]);
+                }
+                else
+                {
+                    TempData["DataStatus"] = "Data not available";
+                }
+            }
+            if (blotterGH.Count < 1)
+                ViewData["DataStatus"] = "Data Not Availavle";
+            ViewBag.Title = "All Blotter Setup";
+            var PAccess = Session["CurrentPagesAccess"].ToString().Split('~');
+            UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), JsonConvert.SerializeObject(blotterGH), this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+
+            ViewData["isDateChangable"] = Convert.ToBoolean(PAccess[2]);
+            ViewData["isEditable"] = Convert.ToBoolean(PAccess[3]);
+            ViewData["IsDeletable"] = Convert.ToBoolean(PAccess[4]);
+            return PartialView("_BlotterGH", blotterGH);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            GazettedHoliday model = new GazettedHoliday();
+            try
+            {
+                UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), "", this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+
+                var ActiveAction = RouteData.Values["action"].ToString();
+                var ActiveController = RouteData.Values["controller"].ToString();
+                Session["ActiveAction"] = ActiveController;
+                Session["ActiveController"] = ActiveAction;
+
+                if (ModelState.IsValid)
+                {
+                    model.createDate = DateTime.Now.Date;
+                }
+            }
+            catch (Exception ex) { }
+
+            return PartialView("_Create", model);
+
+        }
+
+        public ActionResult Create(FormCollection form)
+        {
+            try
+            {
+                UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), "", this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+
+                #region Added by shakir (Currency parameter)
+
+                var selectCurrency = (dynamic)null;
+                if (form["selectCurrency"] != null)
+                    selectCurrency = Convert.ToInt32(form["selectCurrency"].ToString());
+                else
+                    selectCurrency = Convert.ToInt32(Session["SelectedCurrency"].ToString());
+                UtilityClass.GetSelectedCurrecy(selectCurrency);
+
+                #endregion
+
+                return PartialView("_Create");
+            }
+            catch (Exception ex) { }
+
+            return PartialView("_Create");
+        }
+
+        // POST: BlotterGH/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult _Create(GazettedHoliday BlotterGH, FormCollection form)
+        {
+            try
+            {
+                #region Added by shakir (Currency parameter)
+                var selectCurrency = (dynamic)null;
+                if (form["selectCurrency"] != null)
+                    selectCurrency = Convert.ToInt32(form["selectCurrency"].ToString());
+                else
+                    selectCurrency = Convert.ToInt32(Session["SelectedCurrency"].ToString());
+
+                UtilityClass.GetSelectedCurrecy(selectCurrency);
+                #endregion
+
+                if (ModelState.IsValid)
+                {
+                    BlotterGH.UserID = Convert.ToInt16(Session["UserID"].ToString());
+                    BlotterGH.createDate = DateTime.Now;
+                    ServiceRepository serviceObj = new ServiceRepository();
+                    HttpResponseMessage response = serviceObj.PostResponse("api/GazettedHolidays/InsertGH", BlotterGH);
+                    response.EnsureSuccessStatusCode();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                        var JsonLinq = JObject.Parse(jsonResponse);
+                        WebApiResponse getreponse = new WebApiResponse();
+                        getreponse.Status = Convert.ToBoolean(JsonLinq["Status"]);
+                        getreponse.Message = JsonLinq["Message"].ToString();
+                        getreponse.Data = JsonLinq["Data"].ToString();
+
+                        if (getreponse.Status == true)
+                        {
+                            TempData["DataStatus"] = getreponse.Message;
+                        }
+                        else
+                        {
+                            TempData["DataStatus"] = getreponse.Message;
+                        }
+                    }
+                    UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), JsonConvert.SerializeObject(BlotterGH), this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+                    return RedirectToAction("GazettedHoliday");
+                }             
+            }
+            catch (Exception ex) { }
+
+            return PartialView("_Create", BlotterGH);
+        }
+        public ActionResult Edit(int id, FormCollection form)
+        {
+            #region Added by shakir (Currency parameter)
+            var selectCurrency = (dynamic)null;
+            if (form["selectCurrency"] != null)
+                selectCurrency = Convert.ToInt32(form["selectCurrency"].ToString());
+            else
+                selectCurrency = Convert.ToInt32(Session["SelectedCurrency"].ToString());
+
+            UtilityClass.GetSelectedCurrecy(selectCurrency);
+            #endregion
+
+
+            ServiceRepository serviceObj = new ServiceRepository();
+            HttpResponseMessage response = serviceObj.GetResponse("/api/GazettedHolidays/GetBlotterGH?id=" + id.ToString());
+            response.EnsureSuccessStatusCode();
+            // Models.GazettedHoliday BlotterGH = response.Content.ReadAsAsync<Models.GazettedHoliday>().Result;
+            Models.GazettedHoliday BlotterGH = new Models.GazettedHoliday();
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                var JsonLinq = JObject.Parse(jsonResponse);
+                WebApiResponse getreponse = new WebApiResponse();
+                getreponse.Status = Convert.ToBoolean(JsonLinq["Status"]);
+                getreponse.Message = JsonLinq["Message"].ToString();
+                getreponse.Data = JsonLinq["Data"].ToString();
+
+                if (getreponse.Message == "Success")
+                {
+                    JavaScriptSerializer ser = new JavaScriptSerializer();
+                    Dictionary<string, dynamic> ResponseDD = ser.Deserialize<Dictionary<string, dynamic>>(JsonLinq.ToString());
+                    BlotterGH = JsonConvert.DeserializeObject<Models.GazettedHoliday>(ResponseDD["Data"]);
+                }
+            }
+            UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), JsonConvert.SerializeObject(BlotterGH), this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+            return PartialView("_Edit", BlotterGH);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(Models.GazettedHoliday BlotterGH, FormCollection form)
+        {
+            BlotterGH.UserID = Convert.ToInt16(Session["UserID"].ToString());
+            BlotterGH.UpdateDate = DateTime.Now;
+            ServiceRepository serviceObj = new ServiceRepository();
+            HttpResponseMessage response = serviceObj.PutResponse("api/GazettedHolidays/UpdateGH", BlotterGH);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                var JsonLinq = JObject.Parse(jsonResponse);
+                WebApiResponse getreponse = new WebApiResponse();
+                getreponse.Status = Convert.ToBoolean(JsonLinq["Status"]);
+                getreponse.Message = JsonLinq["Message"].ToString();
+                getreponse.Data = JsonLinq["Data"].ToString();
+
+                if (getreponse.Status == true)
+                {
+                    TempData["DataStatus"] = getreponse.Message;
+                }
+                else
+                {
+                    TempData["DataStatus"] = getreponse.Message;
+                }
+            }
+            UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), JsonConvert.SerializeObject(BlotterGH), this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+            return RedirectToAction("GazettedHoliday");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ServiceRepository serviceObj = new ServiceRepository();
+            HttpResponseMessage response = serviceObj.DeleteResponse("api/GazettedHolidays/DeleteGH?id=" + id.ToString());
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                var JsonLinq = JObject.Parse(jsonResponse);
+                WebApiResponse getreponse = new WebApiResponse();
+                getreponse.Status = Convert.ToBoolean(JsonLinq["Status"]);
+                getreponse.Message = JsonLinq["Message"].ToString();
+                getreponse.Data = JsonLinq["Data"].ToString();
+
+                if (getreponse.Status == true)
+                {
+                    TempData["DataStatus"] = getreponse.Message;
+                }
+                else
+                {
+                    TempData["DataStatus"] = getreponse.Message;
+                }
+            }
+            UtilityClass.ActivityMonitor(Convert.ToInt32(Session["UserID"]), Session.SessionID, Request.UserHostAddress.ToString(), new Guid().ToString(), JsonConvert.SerializeObject(id), this.RouteData.Values["action"].ToString(), Request.RawUrl.ToString());
+            return RedirectToAction("GazettedHoliday");
+        }
+    }
+}
---
+++ WebBlotter/Models/GazettedHoliday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBlotter.Models
+{
+    public class GazettedHoliday
+    {
+        public int GHID { get; set; }
+        public string HolidayTitle { get; set; }
+        [Required]
+        public string GHDescription { get; set; }
+
+        [Required]
+        [DisplayName("Value Date")]
+        [DataType(DataType.Date, ErrorMessage = "Date only")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public System.DateTime GHDate { get; set; }
+        public Nullable<System.DateTime> createDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public int UserID { get; set; }
+    }
+}
